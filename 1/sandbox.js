a72617142436bd68f499f6e2d44484a9b8dc20b3
Halimeh Nawrozi <nahalimeh@gmail.com>
2025-05-10 21:24:38 +0330

    Add files via upload

---
+++ 1/sandbox.js
@@ -0,0 +1,8 @@
+//هرچیزی که تا الان توی js یادگرفتیم رو میتونیم اینجا بنویسیم
+var charc = "ali";
+console.log(charc);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (inpute) {
+    console.log(inpute);
+});
---
+++ 1/sandbox.ts
@@ -0,0 +1,10 @@
+//هرچیزی که تا الان توی js یادگرفتیم رو میتونیم اینجا بنویسیم
+const charc = "ali";
+console.log(charc);
+
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
+
+inputs.forEach((inpute) => {
+  console.log(inpute);
+});
